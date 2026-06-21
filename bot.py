abe0c947914d5043e4d410be70b8795124b8954c
이서후 <seohu6640@gmail.com>
2026-06-21 13:00:13 +0900

    feat: setup app command tree with user install settings

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord import app_commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,6 +10,10 @@ class FuneralClient(discord.Client):
     def __init__(self):
         intents = discord.Intents.default()
         super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        await self.tree.sync()
 
     async def on_ready(self):
         pass
